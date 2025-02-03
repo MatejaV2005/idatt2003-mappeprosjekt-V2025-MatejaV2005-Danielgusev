e9a0de4cc966799d2238866c03036849a648aa49
Daniel Gusev <danielgusev04@gmail.com>
2025-02-03 16:02:41 +0100

    First test commit just for fun and to test if the clone is properly working with our git

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -1,8 +1,25 @@
 package edu.ntnu.idi.idatt;
 
+import java.util.Random;
+
 public class Main {
 
+  public static void AreYouGay() {
+    boolean gay;
+    gay = false;
+    Random random = new Random();
+    int number = random.nextInt(10) + 1;
+    if (number > 4) {
+      gay = true;}
+
+    if (gay) {
+      System.out.println("You are gay");
+    } else {
+      System.out.println("You are not gay");
+    }
+  }
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    AreYouGay();
   }
 }
