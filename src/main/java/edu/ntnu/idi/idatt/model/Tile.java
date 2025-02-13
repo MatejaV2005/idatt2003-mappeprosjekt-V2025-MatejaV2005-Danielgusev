f7e8f4157629b6436a52f1d02f29c0cc5293e431
MatejaV2005 <matejav@stud.ntnu.no>
2025-02-13 15:49:55 +0100

    Started working on tile class. no implementation yet, just get methods

--- src/main/java/edu/ntnu/idi/idatt/model/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/model/Tile.java
@@ -2,12 +2,41 @@ package edu.ntnu.idi.idatt.model;
 
 public class Tile {
 
- /*private int tileid;
+ private int tileid;
  private Tile nexttile;
+ private TileAction landaction;
 
   public Tile(int tileid) {
     this.tileid = tileid;
-    nexttile
   }
 
-*/}
+  // Get methods ------------------------------------
+  public Tile getNextTile() {
+    return nexttile;
+  }
+
+  public int getTileId() {
+   return tileid;
+  }
+
+ public TileAction getLandaction() {
+  return landaction;
+ }
+
+ // Set methods --------------------------------------
+
+
+
+  public void setNextTile(Tile tile) {
+  }
+}
+
+
+/*
+* how move, PlaceOnTile and LandPLayer correspond
+
+Move will calculate the destination tile based on the steps its given. it will iterate through the tiles until it has finished the loop and found which tile the player is supposed to go to.
+
+PlaceOnTile Registers the actual tile that the player moved to, and saves the player position to that corresponding tile.
+
+landPlayer tells the tile (if there is an action on it) to activate its action and perform it*/
