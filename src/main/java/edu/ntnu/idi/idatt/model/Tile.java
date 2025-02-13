5d6d71606b37a054085544cea776014c66910e8c
Daniel Gusev <danielgusev04@gmail.com>
2025-02-13 15:53:30 +0100

    Test commit

--- src/main/java/edu/ntnu/idi/idatt/model/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/model/Tile.java
@@ -2,12 +2,21 @@ package edu.ntnu.idi.idatt.model;
 
 public class Tile {
 
- /*private int tileid;
+ private int tileid;
  private Tile nexttile;
+ //private TileAction landAction;
 
   public Tile(int tileid) {
     this.tileid = tileid;
-    nexttile
   }
 
-*/}
+  public int getTileid() {
+    return tileid;
+  }
+
+  public Tile getNexttile() {
+    return nexttile;
+  }
+
+
+}
