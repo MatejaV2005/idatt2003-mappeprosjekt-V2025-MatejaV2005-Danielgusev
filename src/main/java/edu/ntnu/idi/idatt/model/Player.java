9db6a9585134c95a1e96ea06039af4f53ae86619
Daniel Gusev <danielgusev04@gmail.com>
2025-02-13 13:53:48 +0100

    Test commit, unversioned files

--- src/main/java/edu/ntnu/idi/idatt/model/Player.java
+++ src/main/java/edu/ntnu/idi/idatt/model/Player.java
@@ -2,4 +2,20 @@ package edu.ntnu.idi.idatt.model;
 
 public class Player {
 
+  String name;
+  Tile currentTile;
+
+  public Player (String name, BoardGame game) {
+    this.name = name;
+    this.currentTile = game.getBoard().getTile(1);
+  }
+
+  public String getName() {
+    return name;
+  }
+  public Tile getCurrentTile() {
+    return currentTile;
+  }
+
+  public void PlaceOnTile(Tile tile) {}
 }
--- src/main/java/edu/ntnu/idi/idatt/model/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/model/Tile.java
@@ -2,4 +2,12 @@ package edu.ntnu.idi.idatt.model;
 
 public class Tile {
 
-}
+ /*private int tileid;
+ private Tile nexttile;
+
+  public Tile(int tileid) {
+    this.tileid = tileid;
+    nexttile
+  }
+
+*/}
