80dc09275b024a68bb5ba2ec30bdc7f575e3aca4
MatejaV2005 <matejav@stud.ntnu.no>
2025-02-22 14:58:55 +0100

    started working on test classes

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,19 @@
+import edu.ntnu.idi.idatt.model.Tile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TileTest {
+  private Tile tile;
+
+
+  @BeforeEach
+  void setUp() {
+    tile = new Tile(1);
+  }
+
+  @Test
+  void testTileInit() {
+
+  }
+
+}
