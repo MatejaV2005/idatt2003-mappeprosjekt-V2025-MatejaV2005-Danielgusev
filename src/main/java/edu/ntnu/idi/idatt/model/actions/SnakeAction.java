72dc05d6c5c598a556e13d27150eb8f8d7ff6922
MatejaV2005 <matejav@stud.ntnu.no>
2025-03-06 13:02:43 +0100

    Made SnakeAction class

---
+++ src/main/java/edu/ntnu/idi/idatt/model/actions/SnakeAction.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idi.idatt.model.actions;
+
+import edu.ntnu.idi.idatt.model.player_type.Player;
+
+public class SnakeAction implements TileAction{
+
+  @Override
+  public void perform(Player player) {
+
+  }
+
+}
