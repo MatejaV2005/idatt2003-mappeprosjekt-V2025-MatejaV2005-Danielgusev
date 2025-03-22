eefd2b2680729dc5bccb29087847c15e3f50e628
MatejaV2005 <matejav@stud.ntnu.no>
2025-03-22 15:14:30 +0100

    Added methods to TileActionFactory

--- src/main/java/edu/ntnu/idi/idatt/factory/TileActionFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/factory/TileActionFactory.java
@@ -7,11 +7,11 @@ import edu.ntnu.idi.idatt.model.actions.TileAction;
 
 public class TileActionFactory {
 
-  public TileAction createLadderAction(Tile destinationTile, String description) {
+  public static TileAction createLadderAction(Tile destinationTile, String description) {
     return new LadderAction(destinationTile, description);
   }
 
-  public TileAction createSnakeAction(Tile destinationTile, String description) {
+  public static TileAction createSnakeAction(Tile destinationTile, String description) {
     return new SnakeAction(destinationTile, description);
   }
 
