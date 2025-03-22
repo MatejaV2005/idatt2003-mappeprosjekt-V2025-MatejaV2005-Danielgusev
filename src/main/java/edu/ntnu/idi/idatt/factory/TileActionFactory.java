09983cc31d182b582c80ce37de749a2cfe698693
MatejaV2005 <matejav@stud.ntnu.no>
2025-03-22 15:36:28 +0100

    Made CreateSpecialAction method.

--- src/main/java/edu/ntnu/idi/idatt/factory/TileActionFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/factory/TileActionFactory.java
@@ -3,7 +3,10 @@ package edu.ntnu.idi.idatt.factory;
 import edu.ntnu.idi.idatt.model.Tile;
 import edu.ntnu.idi.idatt.model.actions.LadderAction;
 import edu.ntnu.idi.idatt.model.actions.SnakeAction;
+import edu.ntnu.idi.idatt.model.actions.SpecialAction;
 import edu.ntnu.idi.idatt.model.actions.TileAction;
+import edu.ntnu.idi.idatt.model.player_type.Player;
+import java.util.function.Consumer;
 
 public class TileActionFactory {
 
@@ -13,6 +16,10 @@ public class TileActionFactory {
 
   public static TileAction createSnakeAction(Tile destinationTile, String description) {
     return new SnakeAction(destinationTile, description);
+  }
+
+  public static TileAction createSpecialAction(String description, Consumer<Player> action)  {
+    return new SpecialAction(description, action);
   }
 
 
