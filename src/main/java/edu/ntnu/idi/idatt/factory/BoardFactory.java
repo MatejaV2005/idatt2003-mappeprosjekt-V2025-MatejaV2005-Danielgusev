1f7a6f14ac6b5cec717629cd4e775e3c5b936fc7
Daniel Gusev <danielgusev04@gmail.com>
2025-03-22 15:54:54 +0100

    BoardFactory

--- src/main/java/edu/ntnu/idi/idatt/factory/BoardFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/factory/BoardFactory.java
@@ -1,21 +1,167 @@
 package edu.ntnu.idi.idatt.factory;
 
 import edu.ntnu.idi.idatt.model.Board;
+import edu.ntnu.idi.idatt.model.Tile;
+import edu.ntnu.idi.idatt.model.actions.TileAction;
+import edu.ntnu.idi.idatt.utils.ExceptionHandling;
+import java.util.Optional;
 
-  public class BoardFactory {
+public class BoardFactory {
 
-    // Normal difficulty
-    public Board createDefaultBoard() {
-      return new Board(90);
-    }
+  private TileFactory tileFactory;
 
-    // Easy difficulty
-    public Board createSmallBoard() {
-        return new Board(50);
-    }
+  public BoardFactory() {
+    // Instantiate the TileFactory for creating tiles (if needed)
+    this.tileFactory = new TileFactory();
+  }
 
-    // Hard difficulty
-    public Board createBigBoard() {
-      return new Board(120);
-    }
+  /**
+   * Creates an easy board: a small board (50 tiles) with many ladders.
+   *
+   * @return a new Board instance configured as an easy board.
+   */
+  public Board createEasyBoard() {
+    Board board = new Board(50);
+    assignEasyActions(board);
+    return board;
+  }
+
+  /**
+   * Creates a normal board: a default board (90 tiles) with an equal number of ladders and snakes.
+   *
+   * @return a new Board instance configured as a normal board.
+   */
+  public Board createNormalBoard() {
+    Board board = new Board(90);
+    assignNormalActions(board);
+    return board;
+  }
+
+  /**
+   * Creates a hard board: a larger board (120 tiles) with more snakes than ladders.
+   *
+   * @return a new Board instance configured as a hard board.
+   */
+  public Board createHardBoard() {
+    Board board = new Board(120);
+    assignHardActions(board);
+    return board;
+  }
+
+  // Assigns ladder actions to an easy board (small board with many ladders)
+  private void assignEasyActions(Board board) {
+    Tile start = board.getTileById(3);
+    Tile destination = board.getTileById(13);
+    TileAction ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(15);
+    destination = board.getTileById(37);
+    ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(32);
+    destination = board.getTileById(45);
+    ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(28);
+    destination = board.getTileById(20);
+    TileAction snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+  }
+
+  // Assigns an equal number of ladder and snake actions for a normal board (90 tiles)
+  private void assignNormalActions(Board board) {
+    Tile start = board.getTileById(8);
+    Tile destination = board.getTileById(18);
+    TileAction ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(33);
+    destination = board.getTileById(58);
+    ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(63);
+    destination = board.getTileById(86);
+    ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(15);
+    destination = board.getTileById(2);
+    TileAction snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+
+    start = board.getTileById(42);
+    destination = board.getTileById(16);
+    snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+
+    start = board.getTileById(70);
+    destination = board.getTileById(55);
+    snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+  }
+
+  // Assigns fewer ladder actions and more snake actions for a hard board (120 tiles)
+  private void assignHardActions(Board board) {
+    Tile start = board.getTileById(14);
+    Tile destination = board.getTileById(68);
+    TileAction ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(60);
+    destination = board.getTileById(100);
+    ladder = TileActionFactory.createLadderAction(
+        destination, "climbs up to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(ladder));
+
+    start = board.getTileById(15);
+    destination = board.getTileById(2);
+    TileAction snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+
+    start = board.getTileById(13);
+    destination = board.getTileById(1);
+    snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+
+    start = board.getTileById(45);
+    destination = board.getTileById(22);
+    snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+
+    start = board.getTileById(74);
+    destination = board.getTileById(55);
+    snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+
+    start = board.getTileById(98);
+    destination = board.getTileById(40);
+    snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+
+    start = board.getTileById(115);
+    destination = board.getTileById(95);
+    snake = TileActionFactory.createSnakeAction(
+        destination, "slides down to tile " + destination.getTileId());
+    start.setLandAction(Optional.of(snake));
+  }
 }
