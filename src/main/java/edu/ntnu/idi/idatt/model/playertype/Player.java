6cd8fcc61f3e9fe745a7391ac7ffb5b01100e856
MatejaV2005 <matejav@stud.ntnu.no>
2025-03-26 16:18:25 +0100

    Added skipTurn property to Player

--- src/main/java/edu/ntnu/idi/idatt/model/playertype/Player.java
+++ src/main/java/edu/ntnu/idi/idatt/model/playertype/Player.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idi.idatt.model.player_type;
+package edu.ntnu.idi.idatt.model.playertype;
 
 import edu.ntnu.idi.idatt.model.Tile;
 import edu.ntnu.idi.idatt.utils.ExceptionHandling;
@@ -6,10 +6,12 @@ import edu.ntnu.idi.idatt.utils.ExceptionHandling;
 public abstract class Player {
   private String name;
   private Tile currentTile;
+  private boolean skipTurn;
 
   protected Player (String name, Tile startingTile) {
     setName(name);
     this.currentTile = startingTile;
+    this.skipTurn = false;
   }
 
 
@@ -27,6 +29,20 @@ public abstract class Player {
     return currentTile;
   }
 
+  public boolean shouldSkipTurn() {
+    if (skipTurn) {
+      skipTurn = false;
+      return true;
+    }
+    return false;
+  }
+
+  public void setSkipTurn(boolean skipTurn) {
+    this.skipTurn = skipTurn;
+  }
+
+
+
   public abstract void move(int steps);
 
 
@@ -41,7 +57,7 @@ public abstract class Player {
     Tile newTile = currentTile;
 
     for (int i = 0; i < steps; i++) {
-      if(newTile.getNextTile() != null) { //check for if the player is at the end of the board
+      if (newTile.getNextTile() != null) { //check for if the player is at the end of the board
         newTile = newTile.getNextTile();
       }
     }
