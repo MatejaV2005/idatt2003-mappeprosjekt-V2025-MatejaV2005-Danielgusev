1db717df5b93427b0a193fb83eae48c6c1934764
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-05 22:54:02 +0200

    Extended interface for easier serialization

--- src/main/java/edu/ntnu/idi/idatt/model/actions/TileAction.java
+++ src/main/java/edu/ntnu/idi/idatt/model/actions/TileAction.java
@@ -1,13 +1,18 @@
 package edu.ntnu.idi.idatt.model.actions;
 
+import edu.ntnu.idi.idatt.model.ActionType;
+import edu.ntnu.idi.idatt.model.Tile;
 import edu.ntnu.idi.idatt.model.playertype.Player;
 
 public interface TileAction {
 
-  // A static final variable providing a default "noaction" TileAction
-  static final TileAction NO_OP = player -> {};
-
-
+  /**
+   * Perform the action on the given player.
+   *
+   * @param player the player to perform the action on
+   */
   void perform(Player player);
-
+  ActionType getActionType();
+  int getDestinationTileId();
+  String getDescription();
 }
