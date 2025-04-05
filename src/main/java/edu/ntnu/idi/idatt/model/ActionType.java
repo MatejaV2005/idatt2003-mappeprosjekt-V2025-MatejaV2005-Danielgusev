b4cf2b1555708db02375826ebaada899a7146ce3
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-05 22:52:19 +0200

    Refactored ActionType, class now has a constructor which stores a JsonValue (String)

--- src/main/java/edu/ntnu/idi/idatt/model/ActionType.java
+++ src/main/java/edu/ntnu/idi/idatt/model/ActionType.java
@@ -1,10 +1,36 @@
 package edu.ntnu.idi.idatt.model;
 
 public enum ActionType {
-  NO_OP,
-  LADDER,
-  SNAKE,
-  SPECIAL,
-  RETURN_TO_START,
-  SKIP_TURN,
+  LADDER("Ladder"),
+  SNAKE("Snake"),
+  SPECIAL("Special"),
+  RETURN_TO_START("ReturnToStart"),
+  SKIP_TURN("SkipTurn"),
+  NO_OP("NoOp");
+
+
+  private final String jsonValue;
+
+  ActionType(String jsonValue) {
+    this.jsonValue = jsonValue;
+  }
+
+  public String getJsonValue() {
+    return jsonValue;
+  }
+
+  public static ActionType fromJsonValue(String value) {
+    if (value == null) {
+      return NO_OP;
+    }
+
+    for (ActionType type : values()) {
+      if (type.jsonValue.equalsIgnoreCase(value)) {
+        return type;
+      }
+    }
+
+    System.out.println("Unknown action type: " + value + ", defaulting to NO_OP");
+    return NO_OP;
+  }
 }
