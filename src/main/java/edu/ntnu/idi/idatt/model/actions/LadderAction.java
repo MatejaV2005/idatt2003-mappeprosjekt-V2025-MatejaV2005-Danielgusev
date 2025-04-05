84b08ac7d182c5d4de4acad31e89e68c7b3f5648
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-05 22:55:18 +0200

    Added constructor for handling destination tile ID's. implemented new methods

--- src/main/java/edu/ntnu/idi/idatt/model/actions/LadderAction.java
+++ src/main/java/edu/ntnu/idi/idatt/model/actions/LadderAction.java
@@ -1,15 +1,25 @@
 package edu.ntnu.idi.idatt.model.actions;
 
+import edu.ntnu.idi.idatt.model.ActionType;
 import edu.ntnu.idi.idatt.model.Tile;
 import edu.ntnu.idi.idatt.model.playertype.Player;
 
 public class LadderAction implements TileAction {
   private final Tile destinationTile;
   private final String description;
+  private final int destinationTileId;
 
-
+  // Existing constructor
   public LadderAction(Tile destinationTile, String description) {
     this.destinationTile = destinationTile;
+    this.destinationTileId = destinationTile != null ? destinationTile.getTileId() : -1;
+    this.description = description;
+  }
+
+  // New constructor for when we only have an ID
+  public LadderAction(int destinationTileId, String description) {
+    this.destinationTile = null;
+    this.destinationTileId = destinationTileId;
     this.description = description;
   }
 
@@ -18,5 +28,20 @@ public class LadderAction implements TileAction {
     //TODO: add distinguishing features for
     System.out.println(player.getName() + " " + description);
     player.placeOnTile(destinationTile);
+  }
+
+  @Override
+  public ActionType getActionType() {
+    return ActionType.LADDER;
+  }
+
+  @Override
+  public int getDestinationTileId() {
+    return destinationTile != null ? destinationTile.getTileId() : -1;
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
   }
 }
--- src/main/java/edu/ntnu/idi/idatt/model/actions/SnakeAction.java
+++ src/main/java/edu/ntnu/idi/idatt/model/actions/SnakeAction.java
@@ -1,15 +1,25 @@
 package edu.ntnu.idi.idatt.model.actions;
 
+import edu.ntnu.idi.idatt.model.ActionType;
 import edu.ntnu.idi.idatt.model.Tile;
 import edu.ntnu.idi.idatt.model.playertype.Player;
 
 public class SnakeAction implements TileAction{
   private final Tile destinationTile;
   private final String description;
+  private final int destinationTileId;
 
 
   public SnakeAction(Tile destinationTile, String description) {
     this.destinationTile = destinationTile;
+    this.destinationTileId = destinationTile != null ? destinationTile.getTileId() : -1;
+    this.description = description;
+  }
+
+  // New constructor for when we only have an ID
+  public SnakeAction(int destinationTileId, String description) {
+    this.destinationTile = null;
+    this.destinationTileId = destinationTileId;
     this.description = description;
   }
 
@@ -18,6 +28,21 @@ public class SnakeAction implements TileAction{
     //TODO: Add distinguishing features for class
     System.out.println(player.getName() + " " + description);
     player.placeOnTile(destinationTile);
+  }
+
+  @Override
+  public ActionType getActionType() {
+    return ActionType.SNAKE;
+  }
+
+  @Override
+  public int getDestinationTileId() {
+    return destinationTile != null ? destinationTile.getTileId() : -1;
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
   }
 
 }
