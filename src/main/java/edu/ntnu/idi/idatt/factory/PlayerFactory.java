1ec83bf112f5352511d71ff170a44299700ace6c
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-09 16:31:25 +0200

    started working on playerFactory class

---
+++ src/main/java/edu/ntnu/idi/idatt/factory/PlayerFactory.java
@@ -0,0 +1,15 @@
+package edu.ntnu.idi.idatt.factory;
+
+import edu.ntnu.idi.idatt.model.Tile;
+
+public class PlayerFactory {
+
+  public static void createPlayer(String name, String pieceType) {
+
+  }
+
+  public static void createPlayer(String name, String pieceType, Tile currentTile) {
+
+  }
+
+}
