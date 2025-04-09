d8b142307266157136c81041d4ae9184b7670ec6
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-09 20:21:12 +0200

    Late commit! Refactored BoardJsonFileHandler to correspond with boardDTO and boardFactory class

--- src/main/java/edu/ntnu/idi/idatt/filehandler/BoardJsonFileHandler.java
+++ src/main/java/edu/ntnu/idi/idatt/filehandler/BoardJsonFileHandler.java
@@ -2,6 +2,9 @@ package edu.ntnu.idi.idatt.filehandler;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.ntnu.idi.idatt.DataTransfer.BoardDto;
+import edu.ntnu.idi.idatt.converter.BoardConverter;
+import edu.ntnu.idi.idatt.factory.BoardFactory;
 import edu.ntnu.idi.idatt.model.Board;
 
 import java.io.FileReader;
@@ -11,12 +14,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BoardJsonFileHandler implements FileHandler {
-
   private static final Logger logger = Logger.getLogger(BoardJsonFileHandler.class.getName());
   private final Gson gson;
 
   public BoardJsonFileHandler() {
-    this.gson = new Gson();
+    // Configure Gson to handle complex objects
+    this.gson = new GsonBuilder()
+        .setPrettyPrinting()
+        .create();
     logger.setLevel(Level.INFO);
   }
 
@@ -26,8 +31,13 @@ public class BoardJsonFileHandler implements FileHandler {
       throw new IllegalArgumentException("File path cannot be null or empty");
     }
 
+    if (!(object instanceof Board board)) {
+      throw new IllegalArgumentException("Object must be of type Board");
+    }
+
+    BoardDto dto = BoardConverter.toDto(board);
     try (FileWriter writer = new FileWriter(filePath)) {
-      gson.toJson(object, writer);
+      gson.toJson(dto, writer);
       logger.info("Successfully saved board to file: " + filePath);
     } catch (IOException e) {
       logger.log(Level.SEVERE, "Failed to save board to file: " + filePath, e);
@@ -42,9 +52,13 @@ public class BoardJsonFileHandler implements FileHandler {
     }
 
     try (FileReader reader = new FileReader(filePath)) {
-      Board board = gson.fromJson(reader, Board.class);
-      logger.info("Successfully loaded board from file: " + filePath);
-      return board;
+      BoardFactory factory = new BoardFactory();
+      BoardDto dto = gson.fromJson(reader, BoardDto.class);
+
+
+      logger.info("Successfully loaded and relinked board from file: " + filePath);
+      return factory.createBoardFromDto(dto);
+
     } catch (IOException e) {
       logger.log(Level.SEVERE, "Failed to load board from file: " + filePath, e);
       throw new RuntimeException("Failed to load file", e);
