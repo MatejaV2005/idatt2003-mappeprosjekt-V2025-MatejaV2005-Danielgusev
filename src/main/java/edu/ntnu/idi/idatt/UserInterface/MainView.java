c2f1427995abe5ea0a6099ace0e4a3b74ce2876f
Daniel Gusev <danielgusev04@gmail.com>
2025-04-10 12:44:22 +0200

    Created a mainview class and redirected resource files

---
+++ src/main/java/edu/ntnu/idi/idatt/UserInterface/MainView.java
@@ -0,0 +1,9 @@
+package edu.ntnu.idi.idatt.UserInterface;
+
+import edu.ntnu.idi.idatt.UserInterface.TitleScreen.TitleScreen;
+
+public class MainView {
+  public static void main(String[] args) {
+    TitleScreen.main(args);
+  }
+}
