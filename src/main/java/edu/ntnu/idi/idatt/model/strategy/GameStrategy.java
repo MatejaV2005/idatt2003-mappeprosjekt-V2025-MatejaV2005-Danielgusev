65956ee327456232b9732ca06c47f335c0948b8e
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-23 20:20:51 +0200

    Developed a GameStrategy class, effective handling of game logic

---
+++ src/main/java/edu/ntnu/idi/idatt/model/strategy/GameStrategy.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.idatt.model.strategy;
+
+import edu.ntnu.idi.idatt.model.core.Board;
+import edu.ntnu.idi.idatt.model.playertype.Player;
+import java.util.List;
+
+public interface GameStrategy {
+  boolean checkWinCondition(Player player);
+  void executePlayerTurn(Player player);
+  Player determineWinner(List<Player> player);
+  void InitializeGame(Board board, List<Player> player);
+
+}
