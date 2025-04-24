18598c40594533ab12c18ca3ae2e1a767669f93f
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-24 16:32:13 +0200

    New main application

---
+++ src/main/java/edu/ntnu/idi/idatt/view/MainApplication.java
@@ -0,0 +1,41 @@
+package edu.ntnu.idi.idatt.view;
+
+import edu.ntnu.idi.idatt.controller.NavigationController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+  @Override
+  public void start(Stage primaryStage) {
+    try {
+      // Set up the primary stage
+      primaryStage.setTitle("The Pimp Playground");
+      primaryStage.setMinWidth(800);
+      primaryStage.setMinHeight(600);
+      primaryStage.setMaximized(false);
+
+      // Create the navigation controller
+      NavigationController navigationController = new NavigationController(primaryStage);
+
+      // Navigate to the title screen to start the application
+      navigationController.navigateToTitleScreen();
+
+      // Show the stage
+      primaryStage.show();
+
+    } catch (Exception e) {
+      System.err.println("Error starting application: " + e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Main method to launch the application.
+   *
+   * @param args Command line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
+
