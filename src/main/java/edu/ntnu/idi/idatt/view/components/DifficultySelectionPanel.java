571b6e260dd147b37237bf007c215b42972e7b6a
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-26 18:27:44 +0200

    Making different components for SetupView

---
+++ src/main/java/edu/ntnu/idi/idatt/view/components/DifficultySelectionPanel.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idi.idatt.view.components;
+
+import javafx.scene.layout.VBox;
+
+public class DifficultySelectionPanel extends VBox {
+
+}
---
+++ src/main/java/edu/ntnu/idi/idatt/view/components/GameInfoPanel.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idi.idatt.view.components;
+
+import javafx.scene.layout.VBox;
+
+public class GameInfoPanel extends VBox {
+
+}
