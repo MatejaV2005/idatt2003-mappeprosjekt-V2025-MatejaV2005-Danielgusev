6821ee30ddf445812c0f88f1f32ed2651c0c5690
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-26 18:27:17 +0200

    Started work on GameSetupView

---
+++ src/main/java/edu/ntnu/idi/idatt/view/screens/GameSetupView.java
@@ -0,0 +1,31 @@
+package edu.ntnu.idi.idatt.view.screens;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class GameSetupView {
+  private final Scene scene;
+  private final BorderPane root;
+
+  // Major panels (these are reasonable to keep in the main class)
+  private final VBox difficultyPanel;
+  private final VBox playerPanel;
+  private final VBox boardPreviewPanel;
+
+  private final Button startGameButton;
+  private final Button backButton;
+  private final Button createPlayerButton;
+  private final Button[] difficultyButtons;
+  private final Button uploadBoardButton;
+
+
+
+
+
+
+
+
+}
