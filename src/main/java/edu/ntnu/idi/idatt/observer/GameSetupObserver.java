8f0b868eab2c0e9521cdc0749da4e7d1b5569c73
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-28 12:44:59 +0200

    deprecated classes

--- src/main/java/edu/ntnu/idi/idatt/observer/GameSetupObserver.java
+++
@@ -1,9 +0,0 @@
-package edu.ntnu.idi.idatt.observer;
-
-public interface GameSetupObserver {
-  void onGameModeSelected(String gameMode);
-  void onDifficultySelected(String difficulty);
-  void onBoardPreviewUpdated(String boardImagePath);
-  //void onSetupCompleted(GameConfig config);
-
-}
