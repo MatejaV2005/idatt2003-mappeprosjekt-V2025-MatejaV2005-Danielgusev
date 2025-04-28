f33efaa064e8623cabbb18c011999dbd0a4b0c70
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-28 21:41:48 +0200

    refactored PlayerManagementPanel to have more functionality

--- src/main/java/edu/ntnu/idi/idatt/view/components/gameSelection/PlayerManagementPanel.java
+++ src/main/java/edu/ntnu/idi/idatt/view/components/gameSelection/PlayerManagementPanel.java
@@ -1,12 +1,20 @@
 package edu.ntnu.idi.idatt.view.components.gameSelection;
-
+import edu.ntnu.idi.idatt.exceptions.BoardGameResourceException;
 import edu.ntnu.idi.idatt.factory.ButtonFactory;
-import edu.ntnu.idi.idatt.model.playertype.Player;
+import edu.ntnu.idi.idatt.model.core.playertype.Player;
+import edu.ntnu.idi.idatt.view.utils.ResourceLoader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
@@ -14,10 +22,11 @@ import javafx.scene.layout.HBox;
  * Panel for managing players: adding, saving, and switching between saved/current players.
  */
 public class PlayerManagementPanel extends BorderPane {
+  private static final Logger LOGGER = Logger.getLogger(PlayerManagementPanel.class.getName());
 
   private final TabPane playerTabs;
-  private final ListView<String> playerListView; // TODO: Replace String with actual Player later
-  private final ListView<String> savedPlayerListView;
+  private final ListView<Player> playerListView; // TODO: Replace String with actual Player later
+  private final ListView<Player> savedPlayerListView;
   private final Button addPlayerButton;
   private final Button savePlayerButton;
 
@@ -33,13 +42,17 @@ public class PlayerManagementPanel extends BorderPane {
     initializeTabs();
 
     this.playerListView = new ListView<>();
-    playerListView.getItems().addAll("Player 1", "Player 2", "Player 3"); // Dummy data for now
-
     this.savedPlayerListView = new ListView<>();
-    savedPlayerListView.getItems().addAll("Player 4", "Player 5", "Player 6"); // Dummy data for now
+
+    setupPlayerListView(playerListView);
+    setupPlayerListView(savedPlayerListView);
 
     this.addPlayerButton = buttonFactory.createSmallButton("+ Create New Player");
     this.savePlayerButton = buttonFactory.createSmallButton("Save Player");
+
+    addPlayerButton.getStyleClass().add(".button");
+    savePlayerButton.getStyleClass().add(".button");
+
 
     HBox controlsBox = new HBox(10, savePlayerButton, addPlayerButton);
     controlsBox.setAlignment(Pos.CENTER);
@@ -51,17 +64,82 @@ public class PlayerManagementPanel extends BorderPane {
     this.getStyleClass().add("player-management-panel");
   }
 
+
+  private void setupPlayerListView(ListView<Player> listView) {
+    // Make the rows taller
+    listView.setFixedCellSize(50);
+
+    listView.setCellFactory(param -> new ListCell<>() {
+      private final HBox container = new HBox(15);
+      private final ImageView iconView = new ImageView();
+      private final Label nameLabel = new Label();
+
+      {
+        // Setup cell components once
+        iconView.setFitHeight(32);
+        iconView.setFitWidth(32);
+        iconView.setPreserveRatio(true);
+        nameLabel.setStyle("-fx-font-size: 16px; -fx-text-fill: white;");
+
+        container.setAlignment(Pos.CENTER_LEFT);
+        container.setPadding(new Insets(5, 10, 5, 10));
+        container.getChildren().addAll(iconView, nameLabel);
+      }
+
+      @Override
+      protected void updateItem(Player player, boolean empty) {
+        super.updateItem(player, empty);
+
+        if (empty || player == null) {
+          setText(null);
+          setGraphic(null);
+        } else {
+          nameLabel.setText(player.getName());
+          iconView.setImage(loadPlayerIcon(player.getPieceType()));
+          setGraphic(container);
+        }
+      }
+    });
+  }
+
+  /**
+   * Loads a player icon based on piece type with fallback to default.
+   *
+   * @param pieceType The type of piece to load an icon for
+   * @return The loaded image or null if no image could be loaded
+   */
+  private Image loadPlayerIcon(String pieceType) {
+    // Try to load the specific icon
+    try {
+      return ResourceLoader.loadImage("/edu/ntnu/idi/idatt/view/resources/icons/" +
+          pieceType.toLowerCase() + ".png");
+    } catch (BoardGameResourceException e) {
+      LOGGER.log(Level.INFO, "Could not load icon for piece type: " + pieceType, e);
+
+      // Try to load the default icon
+      try {
+        return ResourceLoader.loadImage("/edu/ntnu/idi/idatt/view/resources/icons/default.png");
+      } catch (BoardGameResourceException ex) {
+        LOGGER.log(Level.WARNING, "Could not load default player icon", ex);
+        return null;
+      }
+    }
+  }
+
   private void initializeTabs() {
     Tab currentPlayers = new Tab("Current Players");
     currentPlayers.setClosable(false);
+    currentPlayers.setContent(playerListView);
+
 
     Tab savedPlayers = new Tab("Saved Players");
     savedPlayers.setClosable(false);
+    savedPlayers.setContent(savedPlayerListView);
 
     playerTabs.getTabs().addAll(currentPlayers, savedPlayers);
   }
 
-  // Getter methods for buttons and list view
+  // Getter methods for buttons, list view and tabs
   public Button getAddPlayerButton() {
     return addPlayerButton;
   }
@@ -70,13 +148,40 @@ public class PlayerManagementPanel extends BorderPane {
     return savePlayerButton;
   }
 
-  public ListView<String> getPlayerListView() {
+
+  // Get-methods for retrieving players
+  public ListView<Player> getPlayerListView() {
     return playerListView;
   }
 
+  public ListView<Player> getSavedPlayerListView() {
+    return savedPlayerListView;
+  }
+
+  // In PlayerManagementPanel
+  public void showCurrentPlayers() {
+    setCenter(playerListView);
+  }
+
+  public void showSavedPlayers() {
+    setCenter(savedPlayerListView);
+  }
+
+  public TabPane getPlayerTabs() {
+    return playerTabs;
+  }
+
+  public Tab getCurrentPlayersTab() {
+    return playerTabs.getTabs().getFirst();
+  }
+
+  public Tab getSavedPlayersTab() {
+    return playerTabs.getTabs().get(1);
+  }
+
   // Methods to manipulate the player list
-  public void addPlayer(String playerName) {
-    playerListView.getItems().add(playerName);
+  public void addPlayer(Player player) {
+    playerListView.getItems().add(player);
   }
 
   public void removeSelectedPlayer() {
