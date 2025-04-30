4ba26674a57206322b5279a19c450feb460376e1
MatejaV2005 <matejav@stud.ntnu.no>
2025-04-30 12:10:19 +0200

    FileHandler throws exceptions and extensive documentation

--- src/main/java/edu/ntnu/idi/idatt/filehandler/FileHandler.java
+++ src/main/java/edu/ntnu/idi/idatt/filehandler/FileHandler.java
@@ -1,7 +1,27 @@
 package edu.ntnu.idi.idatt.filehandler;
 
-public interface FileHandler<T> {
-  void saveToFile(T object, String filePath);
+import edu.ntnu.idi.idatt.exceptions.FileLoadException;
+import edu.ntnu.idi.idatt.exceptions.FileSaveException;
 
-  T loadFromFile(String filePath);
+public interface FileHandler<T> {
+
+  /**
+   * Saves an object to a file at the specified path.
+   *
+   * @param object the object to save
+   * @param filePath the path where the file should be saved
+   * @throws FileSaveException if an error occurs during the save operation
+   * @throws IllegalArgumentException if the object or file path is invalid
+   */
+  void saveToFile(T object, String filePath) throws FileSaveException;
+
+  /**
+   * Loads an object from a file at the specified path.
+   *
+   * @param filePath the path of the file to load
+   * @return the loaded object
+   * @throws FileLoadException if an error occurs during the load operation
+   * @throws IllegalArgumentException if the file path is invalid
+   */
+  T loadFromFile(String filePath) throws FileLoadException;
 }
