a9f6d859f0f8b7359dfbdda833f9c2013ddb1bcf
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-02 14:35:51 +0200

    BoardRenderer interface for rendering different boardGames

---
+++ src/main/java/edu/ntnu/idi/idatt/view/renderer/BoardRenderer.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idi.idatt.view.renderer;
+
+import edu.ntnu.idi.idatt.model.core.Board;
+import edu.ntnu.idi.idatt.model.core.Tile;
+import edu.ntnu.idi.idatt.model.core.playertype.Player;
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+
+public interface BoardRenderer {
+
+  void renderInitialBoard(Pane boardPane, Board board);
+
+  Node createPlayerTokenNode(Player player);
+
+  void updatePlayerTokenPosition(Node playerTokenNode, Tile targetTile, Pane boardPane);
+
+  void updateBoardState(Board board);
+
+}
