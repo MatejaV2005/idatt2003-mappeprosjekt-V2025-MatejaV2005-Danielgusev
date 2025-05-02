ca92ac79691fab12434e20e3e677e5135166e9b7
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-02 14:36:12 +0200

    generic boardGameView interface with standard view methods

---
+++ src/main/java/edu/ntnu/idi/idatt/view/screens/BoardGameView.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.idatt.view.screens;
+
+import edu.ntnu.idi.idatt.controller.BoardGameController;
+import edu.ntnu.idi.idatt.model.core.BoardGame;
+import javafx.scene.Scene;
+
+public interface BoardGameView {
+  Scene getScene();
+
+  void setController(BoardGameController controller);
+
+  void update(BoardGame boardGame);
+}
