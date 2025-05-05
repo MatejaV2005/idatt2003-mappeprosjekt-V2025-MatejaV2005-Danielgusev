108c2e3018e00cee1696a0555e3080c5d11cc0b9
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-05 20:00:56 +0200

    developed BoardGameFacotry to handle game creation

--- src/main/java/edu/ntnu/idi/idatt/factory/BoardGameFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/factory/BoardGameFactory.java
@@ -3,6 +3,7 @@ package edu.ntnu.idi.idatt.factory;
 import edu.ntnu.idi.idatt.model.core.Board;
 import edu.ntnu.idi.idatt.model.core.BoardGame;
 import edu.ntnu.idi.idatt.model.core.Dice;
+import edu.ntnu.idi.idatt.model.core.GameType;
 import edu.ntnu.idi.idatt.model.games.SnakesAndLaddersGame;
 import edu.ntnu.idi.idatt.model.strategy.GameStrategy;
 import edu.ntnu.idi.idatt.model.strategy.SnakesAndLaddersStrategy;
@@ -10,13 +11,29 @@ import edu.ntnu.idi.idatt.model.strategy.SnakesAndLaddersStrategy;
 public class BoardGameFactory {
   private final BoardFactory boardFactory = new BoardFactory();
 
-
-  public BoardGame createSnakesAndLaddersGame() {
-    Board board = boardFactory.createNormalBoard();
-    Dice dice = new Dice(2);
-    GameStrategy strategy = new SnakesAndLaddersStrategy(board, dice);
-
-    return new SnakesAndLaddersGame(board, dice, strategy);
+  public BoardGame createGame(GameType gameType, String difficulty) {
+    switch (gameType) {
+      case SNAKES_AND_LADDERS:
+        return createSnakesAndLaddersGame(difficulty);
+      case MONOPOLY:
+        throw new UnsupportedOperationException("Monopoly game not yet implemented");
+      case LUDO:
+        throw new UnsupportedOperationException("Ludo game not yet implemented");
+      default:
+        throw new IllegalArgumentException("Unsupported game type: " + gameType);
+    }
   }
 
+  private BoardGame createSnakesAndLaddersGame(String difficulty) {
+    Board board = switch (difficulty.toLowerCase()) {
+      case "easy" -> boardFactory.createEasyBoard();
+      case "normal" -> boardFactory.createNormalBoard();
+      case "hard" -> boardFactory.createHardBoard();
+      default -> boardFactory.createNormalBoard();
+    };
+
+    Dice dice = new Dice(2);
+    GameStrategy strategy = new SnakesAndLaddersStrategy(board, dice);
+    return new SnakesAndLaddersGame(board, dice, strategy);
+  }
 }
