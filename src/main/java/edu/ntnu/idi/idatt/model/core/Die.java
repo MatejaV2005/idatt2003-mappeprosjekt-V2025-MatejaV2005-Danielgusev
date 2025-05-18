ad624324c83ec50deb9d2ee61cffa07e505db7d8
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-18 19:04:12 +0200

    Added extensive javadoc to Die.class

--- src/main/java/edu/ntnu/idi/idatt/model/core/Die.java
+++ src/main/java/edu/ntnu/idi/idatt/model/core/Die.java
@@ -1,35 +1,62 @@
 package edu.ntnu.idi.idatt.model.core;
 
-import java.util.logging.Logger;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Represents a single, standard six-sided die.
+ * This class allows for rolling the die to generate a random value between 1 and 6 (inclusive)
+ * and retrieving the last rolled value.
+ */
 public class Die {
-  private int lastRolledValue;  // Renamed to follow Java conventions
+  private int lastRolledValue;
+
   private final Random random;
+
   private static final Logger LOGGER = Logger.getLogger(Die.class.getName());
 
-  /*
-  * TODO: fill out javadoc
-  * */
+  /**
+   * Constructs a new {@code Die} object.
+   * Initializes the random number generator. The die has not been "rolled"
+   * initially, so {@link #getLastRolledValue()} will return its default
+   * initialized value (e.g., 0) until {@link #roll()} is called.
+   *
+   *
+   * Consider calling {@link #roll()} within the constructor if a die should
+   * always have a valid initial face value (1-6) upon creation.
+   * For this implementation, an explicit roll is required to set the first value.
+   *
+   */
   public Die() {
-    random = new Random();
-    // roll(); //Initializes the die-instance with a value
+    this.random = new Random();
+    LOGGER.log(Level.FINE, "Die instance created.");
   }
 
-  // Returns the last rolled value
+  /**
+   * Gets the value of the last roll performed on this die.
+   * If {@link #roll()} has not been called yet after construction,
+   * this will return the initial default value (e.g., 0).
+   *
+   * @return The last value rolled, an integer between 1 and 6 if rolled,
+   * or its initial default if not yet rolled.
+   */
   public int getLastRolledValue() {
     return this.lastRolledValue;
   }
 
-  /*
-  * TODO: Fill out javadoc
-  * */
+  /**
+   * Simulates rolling the die, generating a new random value between 1 and 6 (inclusive).
+   * The new value is stored and can be retrieved using {@link #getLastRolledValue()}.
+   *
+   * @return The new value rolled, an integer between 1 and 6.
+   */
   public int roll() {
-    //LOGGER.info("Dice is rolling...");
+    this.lastRolledValue = this.random.nextInt(6) + 1;
 
-    this.lastRolledValue = random.nextInt(6) + 1;  // Corrected range for a six-sided die
-    //LOGGER.info("Rolled value is: " + lastRolledValue);
+    LOGGER.log(Level.FINER, "Die rolled. New value: {0}", this.lastRolledValue);
 
-    return lastRolledValue;
+    return this.lastRolledValue;
   }
+
 }
