c18205852058f203b3854dc99ee6e508e3b05ecc
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-19 23:40:41 +0200

    Player factory creates Player instance now

--- src/main/java/edu/ntnu/idi/idatt/factory/PlayerFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/factory/PlayerFactory.java
@@ -1,13 +1,12 @@
 package edu.ntnu.idi.idatt.factory;
 
 import edu.ntnu.idi.idatt.model.core.Tile;
-import edu.ntnu.idi.idatt.model.core.playertype.HumanPlayer;
 import edu.ntnu.idi.idatt.model.core.playertype.Player;
 
 public class PlayerFactory {
 
   public static Player createPlayer(String name, String pieceType, Tile currentTile) {
-    Player player = new HumanPlayer(name, pieceType);
+    Player player = new Player(name, pieceType);
     player.setOnCurrentTile(currentTile);
 
     return player;
@@ -15,7 +14,7 @@ public class PlayerFactory {
 
   // For creating and saving players only?
   public static Player createPlayer(String name, String pieceType) {
-    return new HumanPlayer(name, pieceType);
+    return new Player(name, pieceType);
   }
 
 
