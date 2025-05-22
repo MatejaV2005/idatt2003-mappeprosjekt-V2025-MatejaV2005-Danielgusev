cee5084c99c000daac7fc303cb601c17851c09e5
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-23 00:10:33 +0200

    Added extensive javadoc

--- src/main/java/edu/ntnu/idi/idatt/view/screens/BoardGameView.java
+++ src/main/java/edu/ntnu/idi/idatt/view/screens/BoardGameView.java
@@ -1,12 +1,33 @@
 package edu.ntnu.idi.idatt.view.screens;
 
 import edu.ntnu.idi.idatt.controller.BoardGameController;
-import edu.ntnu.idi.idatt.model.core.BoardGame;
 import javafx.scene.Scene;
 
+/**
+ * Defines a board game UI in JavaFX.
+ *
+ * <p>Provides the primary {@link Scene} and allows
+ * binding a {@link BoardGameController} to handle user actions
+ * and orchestrate updates.</p>
+ *
+ * @see edu.ntnu.idi.idatt.controller.BoardGameController
+ */
 public interface BoardGameView {
+
+  /**
+   * Retrieves this view’s JavaFX scene.
+   *
+   * @return the configured Scene; never null
+   */
   Scene getScene();
 
+  /**
+   * Sets the controller responsible for handling user input
+   * and driving game logic updates.
+   *
+   * @param controller the BoardGameController; must not be null
+   */
   void setController(BoardGameController controller);
 
 }
+
