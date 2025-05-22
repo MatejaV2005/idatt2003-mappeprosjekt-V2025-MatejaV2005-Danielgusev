7f16ebc22da429e235afa23bdaf60334c4c9fba4
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-22 11:46:38 +0200

    Added extensive javadoc to PlayerFactory

--- src/main/java/edu/ntnu/idi/idatt/factory/PlayerFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/factory/PlayerFactory.java
@@ -1,22 +1,32 @@
 package edu.ntnu.idi.idatt.factory;
 
-import edu.ntnu.idi.idatt.model.core.Tile;
 import edu.ntnu.idi.idatt.model.core.Player;
 
+/**
+ * Factory for creating {@link Player} instances.
+ *
+ * <p>Provides methods to instantiate players with or without an initial
+ * tile placement. Suitable for initializing game sessions or loading
+ * saved players.
+ *
+ * @see Player
+ */
 public class PlayerFactory {
 
-  public static Player createPlayer(String name, String pieceType, Tile currentTile) {
-    Player player = new Player(name, pieceType);
-    player.setOnCurrentTile(currentTile);
-
-    return player;
+  private PlayerFactory() {
   }
 
-  // For creating and saving players only?
+
+  /**
+   * Creates a new player with the specified name and piece type, without setting a tile.
+   *
+   * <p>Use this overload when tile placement is not yet known or will be set later.
+   *
+   * @param name the player's unique name; must not be null or blank
+   * @param pieceType the identifier or color of the player's piece; must not be null or blank
+   * @return a new {@link Player} without initial tile placement
+   */
   public static Player createPlayer(String name, String pieceType) {
     return new Player(name, pieceType);
   }
-
-
-
 }
