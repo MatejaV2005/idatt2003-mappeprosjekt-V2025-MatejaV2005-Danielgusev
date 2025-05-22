a171f5f2caef233e0bbda1ed66a919e502a696ba
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-22 12:34:52 +0200

    added javadoc

--- src/main/java/edu/ntnu/idi/idatt/filehandler/FileHandler.java
+++ src/main/java/edu/ntnu/idi/idatt/filehandler/FileHandler.java
@@ -3,6 +3,17 @@ package edu.ntnu.idi.idatt.filehandler;
 import edu.ntnu.idi.idatt.exceptions.FileLoadException;
 import edu.ntnu.idi.idatt.exceptions.FileSaveException;
 
+/**
+ * Generic interface for persisting and retrieving objects to and from files.
+ *
+ * <p>Implementations serialize an instance of {@code T} to disk (via
+ * {@link #saveToFile(Object, String)}) and reconstruct it (via
+ * {@link #loadFromFile(String)}), wrapping I/O errors in domain-specific
+ * exceptions.</p>
+ *
+ * @param <T> the type of object this handler reads and writes
+ * @since 1.0
+ */
 public interface FileHandler<T> {
 
   /**
