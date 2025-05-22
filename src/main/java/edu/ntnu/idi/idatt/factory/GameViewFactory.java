1ed7ab57bb12516596a616bbc1b1faabfb672244
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-22 11:36:09 +0200

    Added extensive javadoc to GameViewFactory

--- src/main/java/edu/ntnu/idi/idatt/factory/GameViewFactory.java
+++ src/main/java/edu/ntnu/idi/idatt/factory/GameViewFactory.java
@@ -7,23 +7,38 @@ import edu.ntnu.idi.idatt.view.renderer.SnakesAndLaddersRenderer;
 import edu.ntnu.idi.idatt.view.screens.GenericBoardGameView;
 import java.util.Objects;
 
+/**
+ * Factory for creating {@link GenericBoardGameView} instances based on game type.
+ *
+ * <p>Selects and instantiates the appropriate {@link BoardRenderer}
+ * implementation for the given {@link GameType} and wraps it in a
+ * {@link GenericBoardGameView}.
+ *
+ * @see AstroRallyRenderer
+ * @see SnakesAndLaddersRenderer
+ * @since 1.0
+ */
 public class GameViewFactory {
 
+  /**
+   * Creates a {@link GenericBoardGameView} for the specified game type.
+   *
+   * <p>Validates that {@code type} is not null, then chooses the correct
+   * {@link BoardRenderer}. Throws if the game type is unsupported.
+   *
+   * @param type the game type to render; must not be null
+   * @return a new {@link GenericBoardGameView} configured for {@code type}
+   * @throws NullPointerException     if {@code type} is null
+   * @throws IllegalArgumentException if {@code type} is not supported
+   */
   public GenericBoardGameView createViewFor(GameType type) {
     Objects.requireNonNull(type, "GameType cannot be null in createViewFor");
 
-    BoardRenderer renderer;
-    switch (type) {
-      case SNAKES_AND_LADDERS:
-        renderer = new SnakesAndLaddersRenderer();
-        break;
-      case ASTRO_RALLY:
-        renderer = new AstroRallyRenderer();
-        break;
-      default:
-        throw new IllegalArgumentException("Unsupported or unknown game type: " + type);
-    }
+    BoardRenderer renderer = switch (type) {
+      case SNAKES_AND_LADDERS -> new SnakesAndLaddersRenderer();
+      case ASTRO_RALLY -> new AstroRallyRenderer();
+    };
+
     return new GenericBoardGameView(renderer);
   }
-
 }
