8dd56f479139b49643c22819cc29ac05179cfad6
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-22 08:13:53 +0200

    Added extensive javadoc to TileActionInterface

--- src/main/java/edu/ntnu/idi/idatt/model/core/actions/TileAction.java
+++ src/main/java/edu/ntnu/idi/idatt/model/core/actions/TileAction.java
@@ -2,15 +2,38 @@ package edu.ntnu.idi.idatt.model.core.actions;
 
 import edu.ntnu.idi.idatt.model.core.Player;
 
+/**
+ * Defines the contract for actions performed when a player interacts with a tile.
+ * Implementations define specific behaviors like movement or effects.
+ */
 public interface TileAction {
 
   /**
-   * Perform the action on the given player.
+   * Performs the action on the specified player.
    *
-   * @param player the player to perform the action on
+   * @param player The player to perform the action on.
    */
   void perform(Player player);
+
+  /**
+   * Gets the type of this tile action.
+   *
+   * @return The {@link ActionType} of this action.
+   */
   ActionType getActionType();
+
+  /**
+   * Gets the ID of a destination tile, if applicable.
+   * Returns -1 if the action does not involve moving to a specific tile.
+   *
+   * @return The destination tile ID, or -1 if not applicable.
+   */
   int getDestinationTileId();
+
+  /**
+   * Gets a human-readable description of the action.
+   *
+   * @return A string describing the action.
+   */
   String getDescription();
 }
