15af9b7a8cc73ea58a68e1af5166d1fabe8ebeca
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-23 01:36:08 +0200

    Added extensive javadoc to ButtonDecorator

--- src/main/java/edu/ntnu/idi/idatt/view/decorator/ButtonDecorator.java
+++ src/main/java/edu/ntnu/idi/idatt/view/decorator/ButtonDecorator.java
@@ -2,9 +2,28 @@ package edu.ntnu.idi.idatt.view.decorator;
 
 import javafx.scene.control.Button;
 
-// --- Decorator Pattern Implementation ---
-// The decorator interface for buttons
+/**
+ * Defines an interface for decorating {@link Button} objects.
+ *
+ * <p>This interface is part of the Decorator design pattern, allowing for dynamic
+ * extension of button functionalities or appearances.
+ * </p>
+ *
+ * @see Button
+ * @see HoverEffectDecorator
+ */
 public interface ButtonDecorator {
 
+  /**
+   * Applies a decoration to the given {@link Button}.
+   *
+   * <p>Implementations will modify the button (e.g., add event handlers or change styles)
+   * and return the decorated button.
+   *
+   * @param button The {@link Button} to be decorated. Must not be {@code null}.
+   * @return The decorated {@link Button}.
+   *
+   * @throws NullPointerException if the {@code button} argument is {@code null}.
+   */
   Button decorate(Button button);
 }
