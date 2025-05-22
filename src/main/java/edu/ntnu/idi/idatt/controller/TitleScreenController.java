a599f8cc711dc789c87ef86861a05f899d8a3bd9
MatejaV2005 <matejav@stud.ntnu.no>
2025-05-22 09:24:38 +0200

    Added extensive javadoc to TitleScreenController and adherence to google checkstyle and sonarQube practices

--- src/main/java/edu/ntnu/idi/idatt/controller/TitleScreenController.java
+++ src/main/java/edu/ntnu/idi/idatt/controller/TitleScreenController.java
@@ -1,33 +1,45 @@
 package edu.ntnu.idi.idatt.controller;
 
-
 import edu.ntnu.idi.idatt.view.screens.TitleScreenView;
 
+/**
+ * Controls title‐screen interactions.
+ *
+ * <p>Routes the user to the game selection screen or exits the application based on button clicks.
+ */
 public class TitleScreenController {
   private final TitleScreenView view;
   private final NavigationController navigationController;
 
-  public TitleScreenController(TitleScreenView view, NavigationController navigationController) {
+  /**
+   * Creates a TitleScreenController.
+   *
+   * @param view the title‐screen view; must not be null
+   * @param navigationController the navigation controller; must not be null
+   */
+  public TitleScreenController(
+      TitleScreenView view,
+      NavigationController navigationController) {
     this.view = view;
     this.navigationController = navigationController;
-
     view.setController(this);
   }
 
   /**
-   * Handles the "Choose Gamemode" button click.
-   * Navigates to the game selection screen.
+   * Invoked when the user clicks "Choose Gamemode".
+   *
+   * <p>Transitions to the game selection screen.
    */
   public void onChooseGameMode() {
     navigationController.navigateToGameSelection();
   }
 
   /**
-   * Handles the "Quit Game" button click.
-   * Exits the application.
+   * Invoked when the user clicks "Quit Game".
+   *
+   * <p>Exits the application.
    */
   public void onQuitGame() {
     navigationController.exitApp();
   }
 }
-
